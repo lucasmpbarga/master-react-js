0882203ed7d6acd5f5bd29730a63197a2fa9a369
Lucas Barga <lucasmpbarga@gmail.com>
2020-03-15 03:26:51 -0300

    change prettier to basic configuration

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,19 @@
-import React from "react";
-import "./App.css";
-import Person from "./Components/Person/Person";
+import React from 'react'
+import './App.css'
+import Person from './Components/Person/Person'
 
 function App() {
-  return (
-    <div className="App">
-      <h1>Hi, I'm a React App</h1>
-      <p>This is really working!</p>
-      <Person name="Max" age={20}>
-        My Hobbies racing.
-      </Person>
-      <Person name="Axm" age={21} />
-      <Person name="Xam" age={22} />
-    </div>
-  );
+    return (
+        <div className="App">
+            <h1>Hi, I'm a React App</h1>
+            <p>This is really working!</p>
+            <Person name="Max" age={20}>
+                My Hobbies racing.
+            </Person>
+            <Person name="Axm" age={21} />
+            <Person name="Xam" age={22} />
+        </div>
+    )
 }
 
-export default App;
+export default App
